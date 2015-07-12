3ff2a863a12e0d1c63a1f7ac2897aed33085b1b5
Sam <phreakdawg@gmail.com>
2015-07-12 01:20:55 -0600

    adds add 2 integer proc

--- AssemblyTest/main.cpp
+++ AssemblyTest/main.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 extern "C" {
 	short clear(short x);
+	int add2ints(int x1, int x2);
 }
 
 int main()
 {
+	int x1, x2;
 	cout << "Assembly test program...";
-	cout << "Clear = " << clear(22);
+	cout << "Clear = " << clear(22) << "\n";
+
+	cout << "Enter x1: ";
+	cin >> x1;
+	cout << "Enter x2: ";
+	cin >> x2;
+
+	cout << x1 << " + " << x2 << " = " << add2ints(x1, x2) << endl;
 
 	return EXIT_SUCCESS;
 }
--- AssemblyTest/testassembly.asm
+++ AssemblyTest/testassembly.asm
@@ -1,4 +1,4 @@
-.386              ;Target processor.  Use instructions for Pentium class machines
+.586              ;Target processor.  Use instructions for Pentium class machines
 .MODEL FLAT, C    ;Use the flat memory model. Use C calling conventions
 .STACK            ;Define a stack segment of 1KB (Not required for this example)
 .DATA             ;Create a near data segment.  Local variables are declared after
@@ -13,4 +13,15 @@ clear PROC
 	pop ebp
 	ret 
 clear ENDP 
+
+add2ints PROC
+	xor eax, eax
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp]+8
+	mov ebx, [ebp]+12
+	add eax,ebx
+	pop ebp
+	ret
+add2ints ENDP
 END 
